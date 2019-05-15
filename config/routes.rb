e7dc5e53be8aa5cb1658575da81882e4a0a29593
maxsalemans <mhrsalemans@gmail.com>
2019-05-15 16:30:01 +0200

    As good as done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    # like this because a review is linked to a restaurant
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '06872435',
+    category:     'french'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '068124245212',
+    category:     'italian'
+  },
+  {
+    name:         'Juku Ramen',
+    address:      'Vlakbij Leidse plein',
+    phone_number: '124',
+    category:     'japanese'
+  },
+  {
+    name:         'Donor land',
+    address:      'Kinkerstraat Amsterdam',
+    phone_number: '45212',
+    category:     'belgian'
+  },
+  {
+    name:         'Flora',
+    address:      'Floralaan, Eindhoven',
+    phone_number: '0402516635',
+    category:     'chinese'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
